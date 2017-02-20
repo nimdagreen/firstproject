88896847aece7536a61aa7b81725c9d423cd8664
nimdagreen <daikhethuy06@gmail.com>
2017-02-20 15:10:28 +0700

    first commit

---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public static IEnumerable<Product> GetData(int Count)
+        {
+            var Persons = new List<Product>();
+            for (int i = 1; i <= Count; i++)
+            {
+                Persons.Add(new Product { ID = i, Name = "Person" + i.ToString() });
+            }
+            return Persons;
+        }
+
+    }
+}
